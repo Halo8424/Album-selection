10f79b32f1c3f0cbf7fa5817e5c2a338336476a2
Richard.C <r.conde@live.com>
2021-07-28 10:38:24 -0400

    js comment breakdown of app

---
+++ app.js
@@ -0,0 +1,5 @@
+// Click album 
+// Show user album has been selected, add selected class to album
+// Create an array that adds selected album to the array
+// Calculate total of selected albums
+// When saved btn is clicked show notification with total of saved albums
