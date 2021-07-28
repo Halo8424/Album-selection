0cd776426292610d458528b9d4c18354f1e28ed2
Richard.C <r.conde@live.com>
2021-07-28 12:41:00 -0400

    saved items showing

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 // Global Variables
 const notify = document.querySelector(".notify");
-    console.log(notify);
+console.log(notify);
 const albums = document.querySelectorAll(".album");
-    console.log(albums);
+console.log(albums);
 const itemsTotal = document.querySelector(".items-total");
 const saveBtn = document.querySelector(".button");
 
-
-let albumsSelected=[];
+let albumsSelected = [];
 let i = 0;
-while( i < albums.length){
-    albums[i].onclick = function(e){
-        let albumTitle = this.querySelector(".title").textContent;
-        
-        if(this.classList.contains("selected") !== true){
-            this.classList.add("selected");
-            // pushing album title to array
-            albumsSelected.push(albumTitle);
-            
-        } else{
-            this.classList.remove("selected");
-            // to remove the album title from the array 
-            albumsSelected = albumsSelected.filter(function(item) {
-                return item !== albumTitle
-            });
-        }
-      console.log(albumsSelected);  
+while (i < albums.length) {
+  albums[i].onclick = function (e) {
+    let albumTitle = this.querySelector(".title").textContent;
+
+    if (this.classList.contains("selected") !== true) {
+      this.classList.add("selected");
+      // pushing album title to array
+      albumsSelected.push(albumTitle);
+    } else {
+      this.classList.remove("selected");
+      // to remove the album title from the array
+      albumsSelected = albumsSelected.filter(function (item) {
+        return item !== albumTitle;
+      });
     }
-    
-    i++
+    console.log(albumsSelected);
+  };
+
+  i++;
+}
+
+saveBtn.onclick = function () {
+  itemsTotal.textContent = albumsSelected.length + " items saved";
+  notify.classList.add("active");
+  console.log("saved");
 };
 
-saveBtn.onclick = function(){
-    console.log('saved')
-};
-
-
-// Click album 
+// Click album
 // Show user album has been selected, add selected class to album
 // Create an array that adds selected album to the array
 // Calculate total of selected albums
